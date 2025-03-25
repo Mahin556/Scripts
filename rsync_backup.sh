f566016e634901d452575c6e72e8dcbff3c06f52
Mahin556 <mahinraza556@gmail.com>
2025-03-25 15:29:26 +0530

    convert to the script the take backup of user's home dir daily

--- rsync_backup.sh
+++ rsync_backup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-MAIN_SERVER_SRC="/home"    #/home/-->content of directory ##/home --> directory
-BACKUP_SERVER_DEST="/root/Downloads" 
+MAIN_SERVER_SRC="/home/"    #/home/-->content of directory ##/home --> directory
+BACKUP_SERVER_DEST="/root/Downloads/" 
 
-rsync -az root@system1:${MAIN_SERVER_SRC} /root/Downloads/  #PUSH
-rsync -az /root/Downloads${MAIN_SERVER_SRC} root@system2:${BACKUP_SERVER_DEST}   #PULL
+DATE=$(date "+%d-%m-%y")
+
+rsync -az root@system1:${MAIN_SERVER_SRC} /root/Downloads/backup-${DATE}  #PUSH
+rsync -az /root/Downloads/backup-${DATE} root@system2:${BACKUP_SERVER_DEST}   #PULL
+
+
 
 
 
