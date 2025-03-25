e0851a62405a64d8742da0a83aab46b1342af6a5
Mahin556 <mahinraza556@gmail.com>
2025-03-25 15:36:20 +0530

    command optimize

--- rsync_backup.sh
+++ rsync_backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 MAIN_SERVER_SRC="/home/"    #/home/-->content of directory ##/home --> directory
-BACKUP_SERVER_DEST="/root/Downloads/" 
+BACKUP_SERVER_DEST="/root/Downloads" 
 
 DATE=$(date "+%d-%m-%y")
 
 rsync -az root@system1:${MAIN_SERVER_SRC} /root/Downloads/backup-${DATE}  #PUSH
-rsync -az /root/Downloads/backup-${DATE} root@system2:${BACKUP_SERVER_DEST}   #PULL
+rsync -az ${BACKUP_SERVER_DEST}/backup-${DATE} root@system2:${BACKUP_SERVER_DEST}   #PULL
 
 
 
