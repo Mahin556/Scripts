b58b19e488ae8eb2004284532ceeb96dfdb57caf
root <root@mail.virtual.local>
2025-05-31 10:22:10 +0530

    add logging mechanism

--- disk_space.sh
+++ disk_space.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
+set -x
 DISK=/dev/nvme0n1p2
 USED=$(df -H | grep -v -E 'tmpfs|efivarfs|Filesystem' | grep $DISK | awk '{print $(NF-1)}' | tr -d %)
-TO=<email-address>
+TO=<your-email>
 
 if [[ $USED -gt 80 ]];then
-	echo -e "WARNING!!, DISK SPACE IS LOW\nSPACE LEFT: $(( 100-$USED ))%" | mail -s "DISK SPACE ALERT!!" $TO
+	echo -e "WARNING!!, DISK SPACE IS LOW\nSPACE LEFT: $(( 100-$USED ))%" | tee -a /var/log/disk-space.log | mail -s "DISK SPACE ALERT!!" $TO 
 else
-	echo "ALL OKK"
+	echo "Disk space is file ---> $(( 100-$USED ))% left" | tee -a /var/log/disk-space.log &> /dev/null
 fi
 
 
