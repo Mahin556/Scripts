5d354e6e3339824d04e167c8f57173bf3b7f5a36
root <root@mail.virtual.local>
2025-05-31 10:08:54 +0530

    fix

--- disk_space.sh
+++ disk_space.sh
@@ -2,7 +2,7 @@
 
 DISK=/dev/nvme0n1p2
 USED=$(df -H | grep -v -E 'tmpfs|efivarfs|Filesystem' | grep $DISK | awk '{print $(NF-1)}' | tr -d %)
-TO=mahinraza317@gmail.com
+TO=<email-address>
 
 if [[ $USED -gt 80 ]];then
 	echo -e "WARNING!!, DISK SPACE IS LOW\nSPACE LEFT: $(( 100-$USED ))%" | mail -s "DISK SPACE ALERT!!" $TO
