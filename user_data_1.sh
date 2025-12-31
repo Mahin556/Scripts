23159e011f3bd2122e63d098f461fed3d98337a4
Mahin556 <80610763+Mahin556@users.noreply.github.com>
2025-12-31 11:52:39 +0530

    Create user_data_1.sh

---
+++ user_data_1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+sudo yum makecache
+sudo yum install httpd -y
+
+# Install the AWS CLI
+sudo yum install awscli -y
+
+# Get the instance ID using the instance metadata
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+
+
+# Download the images from S3 bucket
+#aws s3 cp s3://myterraformprojectbucket2023/project.webp /var/www/html/project.png --acl public-read
+
+cat <<EOF > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Portfolio</title>
+  <style>
+    /* Add animation and styling for the text */
+    @keyframes colorChange {
+      0% { color: red; }
+      50% { color: green; }
+      100% { color: blue; }
+    }
+    h1 {
+      animation: colorChange 2s infinite;
+    }
+  </style>
+</head>
+<body>
+  <h1>Terraform Project Server 1</h1>
+  <h2>Instance ID: <span style="color:green">$INSTANCE_ID</span></h2>
+  <p>Welcome to Mahin raza site</p>
+  
+</body>
+</html>
+EOF
+
+
+sudo systemctl start httpd
+sudo systemctl enable httpd
